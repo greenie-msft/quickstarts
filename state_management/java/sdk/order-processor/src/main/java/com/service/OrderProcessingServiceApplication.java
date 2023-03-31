f616c4322a9a7032ca9b612005d8f00f2d9acfa1
Brian Benz <bbenz@microsoft.com>
2023-03-31 15:27:23 -0700

    Update try catch

--- state_management/java/sdk/order-processor/src/main/java/com/service/OrderProcessingServiceApplication.java
+++ state_management/java/sdk/order-processor/src/main/java/com/service/OrderProcessingServiceApplication.java
@@ -16,19 +16,24 @@ public class OrderProcessingServiceApplication {
                 int orderId = i;
                 Order order = new Order();
                 order.setOrderId(orderId);
+                try{               
+                    // Save state into the state store
+                    client.saveState(DAPR_STATE_STORE, String.valueOf(orderId), order).block();
+                    System.out.println("Saving Order: " + order.getOrderId());
 
-                // Save state into the state store
-                client.saveState(DAPR_STATE_STORE, String.valueOf(orderId), order).block();
-                System.out.println("Saving Order: " + order.getOrderId());
+                    // Get state from the state store
+                    State<Order> response = client.getState(DAPR_STATE_STORE, String.valueOf(orderId), Order.class).block();
+                    System.out.println("Java Order: " + response.getValue().getOrderId());
 
-                // Get state from the state store
-                State<Order> response = client.getState(DAPR_STATE_STORE, String.valueOf(orderId), Order.class).block();
-                System.out.println("Java Order: " + response.getValue().getOrderId());
-
-                // Delete state from the state store
-                //client.deleteState(DAPR_STATE_STORE, String.valueOf(orderId)).block();
-                System.out.println("NOT Deleting Order: " + orderId);
-                TimeUnit.MILLISECONDS.sleep(3000);
+                    // Delete state from the state store
+                    //client.deleteState(DAPR_STATE_STORE, String.valueOf(orderId)).block();
+                    System.out.println("NOT Deleting Order: " + orderId);
+                    TimeUnit.MILLISECONDS.sleep(1500);
+                }
+                catch(Exception e){
+                    System.out.println("Failed to save : " + e.getMessage());
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                }
             }
         }
     }
